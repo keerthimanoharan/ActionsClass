55c8542566ccd70748ebb153525679815b454474
keerthimanoharan <100154014+keerthimanoharan@users.noreply.github.com>
2022-03-18 22:56:11 +0530

    Add files via upload

---
+++ SelectableinActions.java
@@ -0,0 +1,30 @@
+package week4.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SelectableinActions {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/selectable.html");
+		driver.manage().window().fullscreen();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));	
+
+WebElement item1 = driver.findElement(By.xpath("//li[text()='Item 1']"));
+Actions build=new Actions(driver);
+Point location=item1.getLocation();
+System.out.println(location);
+int x=location.getX();
+int y=location.getY();
+build.dragAndDropBy(item1, x+20, y+20).perform();
+
+}}
