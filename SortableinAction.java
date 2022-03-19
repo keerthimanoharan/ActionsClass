43a970a72bf62ddfb635b3ad0630c5b591bd3746
keerthimanoharan <100154014+keerthimanoharan@users.noreply.github.com>
2022-03-19 23:20:26 +0530

    Add files via upload

---
+++ SortableinAction.java
@@ -0,0 +1,23 @@
+package week4.day2;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SortableinAction {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/sortable.html");
+		driver.manage().window().fullscreen();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));	
+		
+		WebElement item1 = driver.findElement(By.xpath("//li[text()='Item 1']"));
+		WebElement item3 = driver.findElement(By.xpath("//li[text()='Item 3']"));
+		Actions build1=new Actions(driver);
+		build1.dragAndDrop(item3, item1).perform();
+	}}
